f5ad50a1558cf6eadda5fd9f03a66a8ad943acfa
TKhawns <trinhkhanhctdt@gmail.com>
2024-09-26 10:38:54 +0700

    SCPB for 2 constraints

--- updated_register.py
+++ updated_register.py
@@ -4,7 +4,6 @@ from pysat.solvers import Glucose3
 
 id_variable = 0
 sat_solver = Glucose3()
-cnf = CNF()
 
 def pos_i(i, k, weight):
     if i == 0:
@@ -14,6 +13,9 @@ def pos_i(i, k, weight):
         return min(k, sum_w)
     else:
         return k
+
+def plus_clause(clause):
+    sat_solver.add_clause(clause)
 
 def exactly_k(vars: List[int], weight: List[int], k):
     n = len(vars) - 1
@@ -113,46 +115,46 @@ def atLeast_k(vars: List[int], weight: List[int], k):
     for i in range(1, n):
         for j in range(1, weight[i] + 1):
             if j <= pos_i(i, k, weight):
-                cnf.append([-vars[i], map_register[i][j]])
+                plus_clause([-vars[i], map_register[i][j]])
 
     # (2) R_{i-1,j} -> R_i,j for j = 1 to pos_{i-1}
     for i in range(2, n):
         for j in range(1, pos_i(i - 1, k, weight) + 1):
-            cnf.append([-map_register[i - 1][j], map_register[i][j]])
+            plus_clause([-map_register[i - 1][j], map_register[i][j]])
 
     # (3) X_i ^ R_{i-1,j} -> R_i,j+w_i for j = 1 to pos_{i-1}
     for i in range(2, n):
         for j in range(1, pos_i(i - 1, k, weight) + 1):
             if j + weight[i] <= k and j + weight[i] <= pos_i(i, k, weight):
-                cnf.append([-vars[i], -map_register[i - 1][j], map_register[i][j + weight[i]]])
+                plus_clause([-vars[i], -map_register[i - 1][j], map_register[i][j + weight[i]]])
 
     # (4) ¬X_i ^ ¬R_{i-1,j} -> ¬R_i,j for j = 1 to pos_{i-1}
     for i in range(2, n):
         for j in range(1, pos_i(i - 1, k, weight) + 1):
-            cnf.append([vars[i], map_register[i - 1][j], -map_register[i][j]])
+            plus_clause([vars[i], map_register[i - 1][j], -map_register[i][j]])
 
     # (5) ¬X_i -> ¬R_i,j for j = 1 + pos_{i-1} to pos_i
     for i in range(1, n):
         # if pos_i(i - 1, k, weight) < k:
             for j in range(1 + pos_i(i - 1, k, weight), pos_i(i, k, weight) + 1):
-                cnf.append([vars[i], -map_register[i][j]])
+                plus_clause([vars[i], -map_register[i][j]])
 
     # (6) ¬R_{i-1,j} -> ¬R_i,j+w_i for j = 1 to pos_{i-1}
     for i in range(2, n):
         # if pos_i(i - 1, k, weight) < k:
             for j in range(1, pos_i(i - 1, k, weight) + 1):
                 if j + weight[i] <= k and j + weight[i] <= pos_i(i, k, weight):
-                    cnf.append([map_register[i - 1][j], -map_register[i][j + weight[i]]])
+                    plus_clause([map_register[i - 1][j], -map_register[i][j + weight[i]]])
 
     # (7) R_{n-1,k} v (X_n ^ R_{n-1,k-w_n})
     print(f"n: {n}, k: {k}, weight: {weight[n]}")
     if k > pos_i(n - 1, k, weight):
-        cnf.append([vars[n]])
-        cnf.append([map_register[n - 1][k - weight[n]]])
+        plus_clause([vars[n]])
+        plus_clause([map_register[n - 1][k - weight[n]]])
     else:
-        cnf.append([map_register[n - 1][k], vars[n]])
+        plus_clause([map_register[n - 1][k], vars[n]])
         if k - weight[n] > 0 and k - weight[n] <= pos_i(n - 1, k, weight):
-            cnf.append([map_register[n - 1][k], map_register[n - 1][k - weight[n]]])
+            plus_clause([map_register[n - 1][k], map_register[n - 1][k - weight[n]]])
 
     print(id_variable)
     return id_variable
@@ -163,10 +165,11 @@ def atMost_k(vars: List[int], weight: List[int], k, max_var):
     id_variable = max_var
 
     # Create map_register to hold the auxiliary variables
-    map_register = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+    map_register = [[0 for _ in range(k + 1)] for _ in range(n + n + 2)]
+    print(map_register)
 
-    for i in range(1, n):
-        n_bits = pos_i(i, k, weight)
+    for i in range(n+2, n+n+1):
+        n_bits = pos_i(i-n-1, k, weight)
         for j in range(1, n_bits + 1):
             id_variable += 1
             map_register[i][j] = id_variable
@@ -175,36 +178,34 @@ def atMost_k(vars: List[int], weight: List[int], k, max_var):
     print(map_register)
 
     # (1) X_i -> R_i,j for j = 1 to w_i
-    for i in range(1, n):
-        for j in range(1, weight[i] + 1):
-            if j <= pos_i(i, k, weight):
-                cnf.append([-vars[i], map_register[i][j]])
+    for i in range(n+2, n+n+1):
+        for j in range(1, weight[i-n-1] + 1):
+            if j <= pos_i(i-n, k, weight):
+                plus_clause([-vars[i-n-1], map_register[i][j]])
 
     # (2) R_{i-1,j} -> R_i,j for j = 1 to pos_{i-1}
-    for i in range(2, n):
-        for j in range(1, pos_i(i - 1, k, weight) + 1):
-            cnf.append([-map_register[i - 1][j], map_register[i][j]])
+    for i in range(n+3, 2*n+1):
+        for j in range(1, pos_i(i - 1 - n - 1, k, weight) + 1):
+            plus_clause([-map_register[i][j], map_register[i][j]])
 
     # (3) X_i ^ R_{i-1,j} -> R_i,j+w_i for j = 1 to pos_{i-1}
-    for i in range(2, n):
-        for j in range(1, pos_i(i - 1, k, weight) + 1):
-            if j + weight[i] <= k and j + weight[i] <= pos_i(i, k, weight):
-                cnf.append([-vars[i], -map_register[i - 1][j], map_register[i][j + weight[i]]])
+    for i in range(n+3, 2*n+1):
+        for j in range(1, pos_i(i - 1 - n - 1, k, weight) + 1):
+            if j + weight[i-n-1] <= k and j + weight[i-n-1] <= pos_i(i-n-1, k, weight):
+                plus_clause([-vars[i-n-1], -map_register[i - 1][j], map_register[i][j + weight[i-n-1]]])
 
     # (8) At Most K: X_i -> ¬R_{i-1,k+1-w_i} for i = 2 to n 
-    for i in range(2, n + 1):
-        if k + 1 - weight[i] > 0 and k + 1 - weight[i] <= pos_i(i - 1, k, weight):
-            cnf.append([-vars[i], -map_register[i - 1][k + 1 - weight[i]]])
+    for i in range(n+3, 2*n + 1):
+        if k + 1 - weight[i-n-1] > 0 and k + 1 - weight[i-n-1] <= pos_i(i - 1-n-1, k, weight):
+            plus_clause([-vars[i-n-1], -map_register[i - 1][k + 1 - weight[i-n-1]]])
 
     return n
 
 def print_solution(n):
-
-    sat_solver.append_formula(cnf)
     print(f"Number of clauses: {sat_solver.nof_vars()}")
     print(f"Number of clauses: {sat_solver.nof_clauses()}")
 
-    sat_status = sat_solver.solve()
+    sat_status = sat_solver.solve_limited(expect_interrupt = True)
 
     if sat_status is False:
         print("No solutions found")
@@ -226,10 +227,11 @@ def print_solution(n):
 # 10X1 + 2X2 + 6X3 + 11X4 + 21X5 + 4X6 + 8X7 + 3X8 + 8X9 + 10X10 >= 30
 # max_var = atLeast_k([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 10, 2, 6, 11, 21, 4, 8, 3, 8, 10], 30)
 
-max_var = atLeast_k([0, 1, 2, 3, 4, 5], [0, 30, 30, 30, 30, 30], 40)
+result = atLeast_k([0, 1, 2, 3, 4, 5], [0, 30, 30, 30, 30, 30], 60)
+
 
 # 10X1 + 2X2 + 6X3 + 11X4 + 21X5 + 4X6 + 8X7 + 3X8 + 8X9 + 10X10 <= 20
-atMost_k([0, 1, 2, 3, 4, 5], [0, 10, 5, 30, 40, 60], 40, max_var)
+n = atMost_k([0, 1, 2, 3, 4, 5], [0, 10, 5, 30, 40, 50], 30, result)
 
 
 print_solution(5)
